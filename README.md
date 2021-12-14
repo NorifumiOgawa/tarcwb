55b50fb7b08f81533658cf2b2ce4e024c7f40c9e
Norifumi Ogawa <n@ogaworks.com>
2021-12-14 11:52:17 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,32 @@
+# tarcwb
+
+## What is tarcwb?
+
+'tarcwb' is an English-Japanese wordbook CLI application for reading 'The Art of Readable Code'.
+
+## Installation
+
+```
+% npm i -g tarcwb
+```
+
+## Usage
+
+### 1. Execute the command.
+```
+% tarcwb
+```
+
+### 2. Enter the word you want to look up.
+```
+✔ Input english word › off by one
+P157~ off by one errors: 境界条件
+✔ Input english word › persistent variable
+P130~ persistent variable: 永続的変数
+```
+
+### 3. End with an empty newline.
+
+## Dictionary
+
+Pull requests to add words are welcome!
--- index.js
+++ index.js
@@ -1,18 +1,17 @@
 #!/usr/bin/env node
 
-const { mainModule } = require('process');
-const DATA_FILE = './data/dictionary.txt'
+const DATA_FILE = './dictionary.txt'
 
 async function waitWord () {
-  const { prompt } = require('enquirer');
+  const { prompt } = require('enquirer')
 
   const response = await prompt({
     type: 'input',
     name: 'word',
-    message: 'Input english word:'
-  });
-  const word = response['word']
-  if (word) await searchDictionary(word);
+    message: 'Input english word'
+  })
+  const word = response.word
+  if (word) await searchDictionary(word)
   return (word ? 0 : 1)
 }
 
@@ -21,28 +20,28 @@ async function searchDictionary (word) {
   const readline = require('readline')
   const rs = fs.createReadStream(DATA_FILE)
   const rl = readline.createInterface({
-      input: rs
-  });
+    input: rs
+  })
   let pageNumber
   let counter = 0
   for await (const line of rl) {
-    let lineLc = line.toLowerCase()
-    let wordLc = word.toLowerCase()
+    const lineLc = line.toLowerCase()
+    const wordLc = word.toLowerCase()
     if (line.includes('P') && !line.includes(':')) pageNumber = line
     if (lineLc.includes(wordLc)) {
       counter++
-      console.log(`${pageNumber}~ ${lineLc.trim()}`)
+      console.log(`${pageNumber}~) ${lineLc.slice(1).trim()}`)
     }
   }
-  if (counter == 0) {console.log('not found.')}
+  if (counter === 0) { console.log('not found.') }
 }
 
 async function main () {
   let loopEnd = 0
-  while (loopEnd == 0) {
+  while (loopEnd === 0) {
     loopEnd = await waitWord()
   }
   console.log('bye.')
 }
 
-main();
+main()
